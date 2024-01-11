69c5082d802c25384c703468156236cada155c2c
ryuneng <137076160+ryuneng@users.noreply.github.com>
2024-01-11 15:08:31 +0900

    이전버전 삭제

--- mini_project/mycat_section.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-$(".section2").mouseover(function() {
-$(".section2").css("background-color", "yellow");
-});
-$(".section2").mouseout(function() {
-$(".section2").css("background-color", "yellow");
-});
-});
